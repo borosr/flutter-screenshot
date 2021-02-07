2961743d4d71f16304e9a2a07ed96bd0e39a6806
Robert Boros <robbie13.boros@gmail.com>
2021-02-07 18:12:03 +0100

    use better method for waiting

--- pkg/android/wait.go
+++ pkg/android/wait.go
@@ -1,9 +1,7 @@
 package android
 
 import (
-	"bytes"
 	"errors"
-	"time"
 
 	"github.com/borosr/flutter-screenshot/src/device/types"
 	log "github.com/sirupsen/logrus"
@@ -19,28 +17,7 @@ var (
 )
 
 func (d Device) WaitUntilBooted(instance types.Instance) error {
-	cmd := execute("adb", "-s", "emulator-"+instance.DebugPort, "get-state")
+	cmd := execute("adb", "-s", "emulator-"+instance.DebugPort, "wait-for-device")
 	log.Debugf("Emulator state: Executing cmd: %s", cmd.String())
-	var out bytes.Buffer
-	cmd.Stdout(&out)
-	var ec int
-	var start = time.Now()
-	for {
-		if ec >= maxErrorCount {
-			return ErrWaitingTooManyErrors
-		}
-		if err := cmd.Run(); err != nil {
-			log.Error(err)
-			ec++
-			time.Sleep(time.Second)
-
-			continue
-		}
-
-		if out.String() == deviceState {
-			return nil
-		}
-		time.Sleep(time.Second)
-		log.Debugf("Waiting since %v", time.Since(start))
-	}
+	return cmd.Run()
 }
--- pkg/android/wait_test.go
+++ pkg/android/wait_test.go
@@ -1,8 +1,6 @@
 package android
 
 import (
-	"bytes"
-	"errors"
 	"io"
 	"testing"
 
@@ -16,38 +14,7 @@ func TestDevice_WaitUntilBooted(t *testing.T) {
 	mockExecutable := NewMockExecutable(ctrl)
 	execute = mockExecute(mockExecutable)
 
-	var wout *bytes.Buffer
-	gomock.InOrder(
-		mockExecutable.EXPECT().Run().Do(func() error {
-			wout.WriteString("offline")
-			return nil
-		}),
-		mockExecutable.EXPECT().Run().Do(func() error {
-			wout.Reset()
-			wout.WriteString(deviceState)
-			return nil
-		}),
-		)
-	mockExecutable.EXPECT().String().Return("")
-	mockExecutable.EXPECT().Stdout(gomock.Any()).DoAndReturn(func(w io.Writer) {
-		wout = w.(*bytes.Buffer)
-	})
-
-	if err := d.WaitUntilBooted(types.Instance{}); err != nil {
-		t.Error(err)
-	}
-}
-
-func TestDevice_WaitUntilFailRunOnce(t *testing.T) {
-	d := Device{}
-	ctrl := gomock.NewController(t)
-	mockExecutable := NewMockExecutable(ctrl)
-	execute = mockExecute(mockExecutable)
-
-	gomock.InOrder(
-		mockExecutable.EXPECT().Run().Return(errors.New("failed")),
-		mockExecutable.EXPECT().Run().Return(nil),
-	)
+	mockExecutable.EXPECT().Run().Return(nil)
 	mockExecutable.EXPECT().String().Return("")
 	mockExecutable.EXPECT().Stdout(gomock.Any()).DoAndReturn(func(w io.Writer) {
 		_, _ = w.Write([]byte(deviceState))
@@ -55,24 +22,5 @@ func TestDevice_WaitUntilFailRunOnce(t *testing.T) {
 
 	if err := d.WaitUntilBooted(types.Instance{}); err != nil {
 		t.Error(err)
-	}
-}
-
-func TestDevice_WaitUntilFailMaxTimes(t *testing.T) {
-	d := Device{}
-	ctrl := gomock.NewController(t)
-	mockExecutable := NewMockExecutable(ctrl)
-	execute = mockExecute(mockExecutable)
-
-	mockExecutable.EXPECT().Run().MaxTimes(maxErrorCount + 1).Return(errors.New("failed"))
-	mockExecutable.EXPECT().String().Return("")
-	mockExecutable.EXPECT().Stdout(gomock.Any()).DoAndReturn(func(w io.Writer) {
-		_, _ = w.Write([]byte("offline"))
-	})
-
-	if err := d.WaitUntilBooted(types.Instance{}); err == nil {
-		t.Error("Missing error")
-	} else if !errors.Is(err, ErrWaitingTooManyErrors) {
-		t.Errorf("Error should be %v, instead of %v", ErrWaitingTooManyErrors, err)
 	}
 }
