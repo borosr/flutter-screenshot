460acc7e0359e5b7cde1aaf29f1b973304dfb9db
Robert Boros <robbie13.boros@gmail.com>
2021-02-07 16:08:11 +0100

    add help options to create

--- pkg/android/create.go
+++ pkg/android/create.go
@@ -1,15 +1,16 @@
 package android
 
 import (
-	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/borosr/flutter-screenshot/src/device/types"
 )
 
-var (
-	ErrDeviceNotAbleToCreate = errors.New("android virtual device (AVD) not able to create, please create it for yourself")
+const (
+	errFmtDeviceNotAbleToCreate = "android virtual device (AVD) not able to create, please create it for yourself or choose one of the following [%s]"
 )
 
 func (d Device) Create(s string) (string, types.Kind, error) {
-	return "", 0, ErrDeviceNotAbleToCreate
+	return "", 0, fmt.Errorf(errFmtDeviceNotAbleToCreate, strings.Join(d.Config.Devices, ","))
 }
--- pkg/android/create_test.go
+++ pkg/android/create_test.go
@@ -1,14 +1,18 @@
 package android
 
 import (
-	"errors"
+	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestDevice_Create(t *testing.T) {
 	d := Device{}
 	_, _, err := d.Create("")
-	if !errors.Is(err, ErrDeviceNotAbleToCreate) {
-		t.Errorf("Create should throw the error %v, instead of throwing %v", ErrDeviceNotAbleToCreate, err)
+	if err == nil {
+		t.Fatal("Missing error")
+	}
+	if exp := fmt.Sprintf(errFmtDeviceNotAbleToCreate, strings.Join(d.Config.Devices, ",")); err.Error() != exp {
+		t.Errorf("Create should throw the error %v, instead of throwing %v", exp, err)
 	}
 }
