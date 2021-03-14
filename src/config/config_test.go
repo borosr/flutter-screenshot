a5de1df34def03102db9737706ff643f2f3c2831
Robert Boros <robbie13.boros@gmail.com>
2021-03-14 14:17:41 +0100

    add more tests to src/config package

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
 	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
 )
 
 const (
@@ -52,5 +55,25 @@ func TestReadOnNotMac(t *testing.T) {
 	}
 	if len(read.Devices.IOS) != 0 {
 		t.Fatal("Invalid device count, no iOS device should exist")
+	}
+}
+
+func TestNotExistFile(t *testing.T) {
+	_, err := Read(gofakeit.UUID()+".yaml", false)
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Error("Read should return with error if file not exists")
+	}
+}
+
+func TestInvalidFileFormat(t *testing.T) {
+	temp, err := os.CreateTemp("", gofakeit.UUID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := temp.WriteString("some string..."); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := Read(temp.Name(), false); err == nil {
+		t.Error("Read should return with error if format is invalid")
 	}
 }
